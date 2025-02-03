6cc0dff17c7acfe10539d60cf0d4cbb2bd389504
marcosbrienze6 <m.miguelbrienze@hotmail.com>
2025-02-02 23:51:01 -0300

    working in home layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,8 @@ import { useAuth } from "../context/AuthContext";
 import DashboardCards from "../components/DashboardCards";
 import UserCard from "../components/UserCard";
 import styles from "../styles/Home.module.css";
-import {
-  FaUser,
-  FaCalendarAlt,
-  FaFileInvoice,
-  FaHome,
-  FaQuestionCircle,
-} from "react-icons/fa";
+
+import MyResponsiveLine from "../components/BarChart";
 
 const Home = () => {
   const { isAuthenticated, user } = useAuth();
@@ -17,11 +12,11 @@ const Home = () => {
   const hasRole = (roleId) => user?.user_role_id === roleId;
 
   const adminCards = [
+    { title: "Desempenho Geral", link: "/overall-performance" },
     { title: "Professores", link: "/role/2" },
     { title: "Alunos", link: "/role/3" },
     { title: "Responsáveis", link: "/role/4" },
     { title: "Funcionários", link: "/role/5" },
-    { title: "Desempenho Geral", link: "/overall-performance" },
   ];
 
   const teacherCards = [
@@ -37,9 +32,9 @@ const Home = () => {
   const responsavelCards = [{ title: "Calendário", link: "/calendar" }];
 
   return (
-    <div>
+    <div className={styles.home_wrapper}>
       {isAuthenticated ? (
-        <div>
+        <>
           <div className={styles.home_container}>
             <UserCard />
             {hasRole(1) && <DashboardCards cards={adminCards} />}
@@ -47,7 +42,22 @@ const Home = () => {
             {hasRole(3) && <DashboardCards cards={studentCards} />}
             {hasRole(4) && <DashboardCards cards={responsavelCards} />}
           </div>
-        </div>
+          <div className={styles.search_container}>
+            <input placeholder="Busque por algo" type="search" />
+            <div className={styles.extra_content}>
+              <div>
+                <h2>Mais de 7000 alunos!</h2>
+              </div>
+              <div>
+                <h2>Professores qualificados</h2>
+              </div>
+              <div>
+                <h2>Metodologia moderna e eficaz</h2>
+              </div>
+              <MyResponsiveLine />
+            </div>
+          </div>
+        </>
       ) : (
         <div>
           <h1>Tem nada pra tu aqui não, loga aí</h1>
